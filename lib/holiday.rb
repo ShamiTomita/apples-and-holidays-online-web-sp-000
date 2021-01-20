ea91167b2cf1886f4845381e062ed123563a29ff
Shami Tomita <shamitomita@gmail.com>
2021-01-20 18:36:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,11 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  supplies = []
+  holiday_hash[:winter].each do |holiday, decorations|
+    supplies.push(decorations)
+  end 
+  return supplies 
 end
 
 def all_supplies_in_holidays(holiday_hash)
