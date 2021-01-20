2cb81b6af3c22a49d536caaac316cf2360548d29
Shami Tomita <shamitomita@gmail.com>
2021-01-20 18:45:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts season.to_s.capitalize + ": "
+    puts season.to_s.capitalize + ":"
       season.each do |holiday, decorations|
        puts holiday.to_s.capitalize +": " + decorations.to_s.capitalize
      end 
