8e7cc26cc724de714fe9eae6adb34ce240e04bd3
Shami Tomita <shamitomita@gmail.com>
2021-01-20 19:00:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.map do |season, holiday|
   holiday.map do |holiday, item|
-    holiday if 
+    holiday if item.include?("BBQ")
 end
 
 
